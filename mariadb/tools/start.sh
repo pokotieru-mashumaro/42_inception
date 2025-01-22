22abaa8efd05d0c078d4c3a5f19ad4a2138eb77a
pokotieru-mashumaro <k827umeboshi@gmail.com>
2025-01-22 21:47:05 +0900

    init

--- mariadb/tools/start.sh
+++ mariadb/tools/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# データディレクトリのクリーンアップ
-rm -rf /var/lib/mysql/*
-
 sed -i 's|MYSQL_DATABASE|'${MYSQL_DATABASE}'|g' /tmp/init.sql
 sed -i 's|MYSQL_USER|'${MYSQL_USER}'|g' /tmp/init.sql
 sed -i 's|MYSQL_PASSWORD|'${MYSQL_PASSWORD}'|g' /tmp/init.sql
 sed -i 's|MYSQL_ROOT_PASSWORD|'${MYSQL_ROOT_PASSWORD}'|g' /tmp/init.sql
 
-if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    mysqld --user=mysql --bootstrap < /tmp/init.sql
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+  mysqld_safe
+else
+  mysql_install_db
+  mysqld --init-file="/tmp/init.sql"
 fi
 
-exec mysqld --user=mysql
+exec "$@"
