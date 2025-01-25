eb7ebe49a5f78e4dab41dffa831563e6950287b2
pokotieru-mashumaro <k827umeboshi@gmail.com>
2025-01-25 21:07:44 +0900

    遂に終わったgit add .

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -11,24 +11,19 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --dbname="$MYSQL_DATABASE" \
         --dbuser="$MYSQL_USER" \
         --dbpass="$MYSQL_PASSWORD" \
-        --path='/var/www/html' \
         --allow-root
 
     wp core install --url=$DOMAIN_NAME/ \
         --title=Wordpress \
-        --admin_user=$WP_USER \
-        --admin_password=$WP_USER_PASSWORD \
-        --admin_email=$WP_USER_EMAIL \
+        --admin_user=$WP_SUPERUSER \
+        --admin_password=$WP_SUPERUSER_PASSWORD \
+        --admin_email=$WP_SUPERUSER_EMAIL \
         --skip-email --allow-root
     
-    # Error: The 'kkomatsu' username is already registered. て出たから
-    if ! wp user get $WP_USER --path='/var/www/html' --allow-root > /dev/null 2>&1; then
-        wp user create $WP_USER $WP_USER_EMAIL \
-            --role=author \
-            --user_pass=$WP_USER_PASSWORD \
-            --path='/var/www/html' \
-            --allow-root
-    fi
+    wp user create $WP_USER $WP_USER_EMAIL \
+        --role=author \
+        --user_pass=$WP_USER_PASSWORD \
+        --allow-root
 fi
 
 /usr/sbin/php-fpm8.2 -F
